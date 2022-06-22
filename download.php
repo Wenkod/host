3ba489c20e955b775bf1194134193b94cdcaf603
Wenkod <101545425+Wenkod@users.noreply.github.com>
2022-06-22 09:07:32 -0400

    Update download.php

--- download.php
+++ download.php
@@ -1,22 +1,28 @@
 <?php
 
-$file = "/home/fakeifgv/public_html/uploads.zip";
+$absolutepath = "/home/fakeifgv/public_html/";
 
-if (file_exists($file)) {
-	header('Content-Description: File Transfer');
-	header('Content-Type: application/octet-stream');
-	header('Content-Disposition: attachment; filename='.basename($file));
-	header('Content-Transfer-Encoding: binary');
-	header('Expires: 0');
-	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-	header('Pragma: public');
-	header('Content-Length: ' . filesize($file));
-	flush();
-	readfile($file);
-	exit;
-}
-else {
-	echo "FILE NOT FOUND! :(";
+if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE']))
+{
+	$filename = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+	$file = $absolutepath.$filename;
+	if (file_exists($file)) {
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename='.basename($file));
+		header('Content-Transfer-Encoding: binary');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		header('Pragma: public');
+		header('Content-Length: ' . filesize($file));
+		ob_clean();
+		flush();
+		readfile($file);
+		exit;
+	}
+	else {
+		echo "FILE NOT FOUND! :(";
+	}
 }
 
 ?>
