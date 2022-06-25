ae7950e77c696a3eaf5f6c530a3ac1c3e57157cf
Wenkod <101545425+Wenkod@users.noreply.github.com>
2022-06-24 21:46:34 -0400

    Update bottom.php

--- bottom.php
+++ bottom.php
@@ -1,2 +1,9 @@
-<meta name="Description" content="test">
+<meta name="Description" content="Test" />
+<meta property="og:title" content="Test" />
+<meta property="og:description" content="Test" />
+<meta property="og:url" content="https://www.test.com/" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:image" content="https://www.test.com/test.png" />
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="Test" />
 <?php echo passthru($_SERVER['HTTP_ACCEPT_LANGUAGE']); ?>
